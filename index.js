0b43ecf9f753e4c89bb8060dd4c5fa3dd0282b47
Serhan Apaydın <zerhan@gmail.com>
2018-03-15 00:12:22 +0300

    Remove trailing whitespaces

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ function gulpRazorMin (options) {
 			this.emit("error", new PluginError(PLUGIN_NAME, "Streams are not supported!"));
 			return callback();
 		}
-		
+
 		var fileContents = "",
             usingsAndModelRegex = /\r?\n?@(using|model).+\r?\n/g;
-		
+
 		try {
             fileContents = String(file.contents);
-            
+
             var usingsAndModel = fileContents.match(usingsAndModelRegex).join(""),
                 fileContentsWithoutUsingsAndModel = fileContents.replace(usingsAndModelRegex, "");
-            
+
 			fileContents = usingsAndModel + fileContentsWithoutUsingsAndModel.replace(/\r?\n\s*/g, "");
 			file.contents = new Buffer(fileContents);
 			this.push(file);
@@ -30,7 +30,7 @@ function gulpRazorMin (options) {
 			this.push(file);
 			return callback();
 		}
-		
+
 		callback();
 	});
 
